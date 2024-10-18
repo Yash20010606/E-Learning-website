6212d2f93199b2895a07ca90330a42511f0a523e
Yashini2001661 <yashini.20220168@iit.ac.lk>
2024-10-18 20:06:15 +0530

    create an e-learning website

---
+++ src/components/Banner/Banner2.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import BannerPng from "../../assets/banner.png";
+import { motion } from "framer-motion";
+
+const Banner2 = () => {
+  return (
+    <section>
+      <div className="container py-14 md:py-24 grid grid-cols-1 md:grid-cols-2 gap-8 space-y-6 md:space-y-0">
+        {/* Banner Text */}
+        <motion.div
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          className="flex flex-col justify-center"
+        >
+          <div className="text-center md:text-left space-y-4 lg:max-w-[450px]">
+            <h1 className="text-4xl font-bold !leading-snug">
+              Join Our Community to Start your Journey
+            </h1>
+            <p className="text-dark2">
+              Become part of a vibrant learning community dedicated to helping
+              you grow and succeed. Whether you're starting your educational
+              journey or looking to advance in your career, we provide the
+              resources, support, and mentorship to guide you every step of the
+              way. Explore a variety of courses tailored to your needs and
+              connect with like-minded individuals who share your passion for
+              knowledge and self-improvement. Your path to success begins here!
+            </p>
+            <a
+              href="https://chat.whatsapp.com/FQSKgJ5f1eIAhlyF5sVym0"
+              className="primary-btn !mt-8"
+            >
+              Join Now
+            </a>
+          </div>
+        </motion.div>
+        {/* Banner Image */}
+        <div className="flex justify-center items-center">
+          <motion.img
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            src={BannerPng}
+            alt=""
+            className="w-[350px] md:max-w-[450px] object-cover drop-shadow"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner2;
---
+++ src/components/Subscribe/Subscribe.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { FaBell } from "react-icons/fa";
+import BgImage from "../../assets/bg.png";
+import { motion } from "framer-motion";
+
+const bgStyle = {
+  backgroundImage: `url(${BgImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
+
+const Subscribe = () => {
+  return (
+    <section className="bg-[#f7f7f7]">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        style={bgStyle}
+        className="container py-24 md:py-48"
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6, ease: "easeInOut" }}
+          className="flex flex-col justify-center"
+        >
+          <div className="text-center space-y-4 lg:max-w-[430px] mx-auto">
+            <h1 className="text-4xl font-bold !leading-snug">
+              450K+ Students are learning from us
+            </h1>
+            <p>
+              Join a growing community of over 450,000 students worldwide who
+              are advancing their skills and careers with our expert-led
+              courses. Our diverse range of educational programs is designed to
+              empower learners at every level, from beginners to professionals,
+              offering real-world applications and hands-on projects. Whether
+              you’re looking to enhance your knowledge or make a career change,
+              our platform provides the tools and support to achieve your goals.
+            </p>
+            <a
+              href=""
+              className="primary-btn !mt-8 inline-flex items-center gap-4 group"
+            >
+              Subscribe Now
+              <FaBell className="group-hover:animate-bounce group-hover:text-lg duration-200" />
+            </a>
+          </div>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Subscribe;
